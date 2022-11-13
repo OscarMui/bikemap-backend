6c04df37fc04604f614fb2bfa90d9b409d2f9534
SJP Math <sjpmath314@gmail.com>
2022-11-13 10:58:18 +0000

    added votes to pins

--- routes/init.js
+++ routes/init.js
@@ -10,6 +10,7 @@ const data = [
         Latitude: 51.752359,
         Title: "Logic Lane",
         Description: "Bike Rack",
+        Votes: ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'],
     },
     {
         User: "admin",
@@ -18,6 +19,7 @@ const data = [
         Latitude: 51.752739,
         Title: "Queen's Lane",
         Description: "Bike Rack",
+        Votes: ['4','5','6','7','8','9','10'],
     },
     {
         User: "admin",
@@ -26,6 +28,7 @@ const data = [
         Latitude: 51.754074,
         Title: "Catte Street",
         Description: "Bike Rack",
+        Votes: ['8','9','10','11','12'],
     },
     {
         User: "admin",
@@ -34,6 +37,7 @@ const data = [
         Latitude: 51.755190,
         Title: "Parks Road",
         Description: "Bike Rack",
+        Votes: ['1','2','3','4','5','6','7','8','9','10'],
     },
     {
         User: "admin",
@@ -42,6 +46,7 @@ const data = [
         Latitude: 51.759707,
         Title: "Keble Road",
         Description: "Bike Rack",
+        Votes: ['13','20','3','4','5','1','2','8','9','10'],
     },
     {
         User: "admin",
@@ -50,6 +55,7 @@ const data = [
         Latitude: 51.759342,
         Title: "Keble Road",
         Description: "Bike Rack",
+        Votes: ['10','11','12','13','14','15','16','17','18','19','20'],
     },
     {
         User: "admin",
@@ -58,6 +64,7 @@ const data = [
         Latitude: 51.754345,
         Title: "Magdalen Street East",
         Description: "Bike Rack",
+        Votes: ['1','3','5','7','9','11','13','15','17','19'],
     },
     {
         User: "admin",
@@ -66,6 +73,7 @@ const data = [
         Latitude: 51.753732,
         Title: "Ship Street",
         Description: "Bike Rack",
+        Votes: ['2','4','6','8','10','12','14','16','18','20'],
     },
     {
         User: "admin",
@@ -74,6 +82,7 @@ const data = [
         Latitude: 51.754031,
         Title: "Longwall Street",
         Description: "Bike Rack",
+        Votes: ['2','3','5','7','9','11','13','17','6'],
     },
     {
         User: "admin",
@@ -82,6 +91,7 @@ const data = [
         Latitude: 51.755443,
         Title: "Jowett Walk",
         Description: "Bike Rack",
+        Votes: ['2','3','6','1'],
     },
     {
         User: "admin",
@@ -94,6 +104,7 @@ const data = [
             "It is located where three key routes from south and east Oxford, where most of the city's population lives, meet with those roads - St Clement's Street, Cowley Road and Iffley Road - then funnelled towards Magdalen Bridge and the city centre.<br />"+
             "A number of the city\'s bus services pass through the roundabout, as do coach services to and from London and other destinations."
         ,
+        Votes: ['1','2','8','3','7','11','12','5'],
     },
     {
         User: "admin",
@@ -104,6 +115,7 @@ const data = [
         Description: "Bike Shop<br />"+
             "<a href='https://goo.gl/maps/Q7qy3kVEPTgFRvEH7'>View on Google Maps</a>"
         ,
+        Votes: ['7','1','2','5','3','17','10','11'],
     },
     {
         User: "admin",
@@ -114,6 +126,7 @@ const data = [
         Description: "Bike Shop<br />"+
             "<a href='https://goo.gl/maps/zPqzAcZLmcKk9pVQ8'>View on Google Maps</a>"
         ,
+        Votes: ['2','5','3','12','8','13','7','1'],
     },
     {
         User: "admin",
@@ -122,6 +135,7 @@ const data = [
         Latitude: 51.760622137160475,
         Title: "The Mathematical Institute",
         Description: "Bike Rack",
+        Votes: ['7','1','12','5','3','17','10','11','2','4','6','8'],
     },
     {
         User: "admin",
@@ -130,6 +144,7 @@ const data = [
         Latitude: 51.75317064274,
         Title: "Magdalen College Back Gate",
         Description: "Bike Rack",
+        Votes: ['1','14','16','17','12','2','10','20','13','3'],
     },
     {
         User: "admin",
@@ -138,6 +153,7 @@ const data = [
         Latitude: 51.750444228243026,
         Title: "Sainsbury's",
         Description: "Bike Rack",
+        Votes: ['13','7','1','20','5','3','17','10','11'],
     },
     {
         User: "admin",
@@ -146,6 +162,7 @@ const data = [
         Latitude: 51.758164504872,
         Title: "Park's Road",
         Description: "Park's Road pedestrian path closed in this section.",
+        Votes: ['1','2','4','6','7','10','11','12','14','20'],
     },
     {
         User: "admin",
@@ -154,6 +171,7 @@ const data = [
         Latitude: 51.753452598467085,
         Title: "Radcliffe Camera",
         Description: "Bike Rack",
+        Votes: ['1','3','5','6','7','8','10','12','14','16','19'],
     },
     {
         User: "admin",
@@ -162,6 +180,7 @@ const data = [
         Latitude: 51.75511335057417,
         Title: "Parks Road",
         Description: "Bike Rack",
+        Votes: ['2','4','5','7','8','10','11','12','18','20'],
     },
     {
         User: "admin",
@@ -170,6 +189,7 @@ const data = [
         Latitude: 51.7540975349175,
         Title: "Broad Street",
         Description: "Bike Rack",
+        Votes: ['1','5','15','10','20','11','13','17','18'],
     },
     {
         User: "admin",
@@ -178,6 +198,7 @@ const data = [
         Latitude: 51.754275777254946,
         Title: "Magdalen Street",
         Description: "Bike Rack",
+        Votes: ['2','3','6','8','9','12','15','18'],
     },
     {
         User: "admin",
@@ -186,6 +207,7 @@ const data = [
         Latitude: 51.75431396602882,
         Title: "Broad Street",
         Description: "Bike Rack",
+        Votes: ['2','4','8','16','1','3','9','5','20'],
     },
     {
         User: "admin",
@@ -194,6 +216,7 @@ const data = [
         Latitude: 51.75428328121007,
         Title: "Blackwell's Art & Poster Shop",
         Description: "Bike Rack",
+        Votes: ['3','4','6','7','8','19','18', '17', '20', '12'],
     },
     {
         User: "admin",
@@ -202,6 +225,7 @@ const data = [
         Latitude: 51.75454580547427,
         Title: "Blackwell's Bookshop",
         Description: "Bike Rack",
+        Votes: ['2','3','4','5','6','7','8','9','10','11','12','13','15','16'],
     },
     {
         User: "admin",
@@ -210,6 +234,7 @@ const data = [
         Latitude: 51.757754524080866,
         Title: "Parks Road S Parks Road Intersection",
         Description: "Bike Rack",
+        Votes: ['5','1','2','3','4','8', '10', '16', '17','19','20','13'],
     },
     {
         User: "admin",
@@ -218,6 +243,7 @@ const data = [
         Latitude: 51.75881735798493,
         Title: "S Parks Road",
         Description: "Bike Rack",
+        Votes: ['2','3','6','1'],
     },
     {
         User: "admin",
@@ -226,6 +252,7 @@ const data = [
         Latitude: 51.75831918604838,
         Title: "S Parks Road",
         Description: "Bike Rack",
+        Votes: ['8','3','7','1','2','5','13','17'],
     },
     {
         User: "admin",
@@ -234,6 +261,7 @@ const data = [
         Latitude: 51.75689387242146,
         Title: "St Cross Road",
         Description: "Bike Rack",
+        Votes: ['12','13','16','11','2','8','3','17','1'],
     },
     {
         User: "admin",
@@ -242,6 +270,7 @@ const data = [
         Latitude: 51.75968073924905,
         Title: "University Parks",
         Description: "Bike Rack",
+        Votes: ['13','7','1','2','5','3','17','10','11'],
     },
     {
         User: "admin",
@@ -250,6 +279,7 @@ const data = [
         Latitude: 51.75193096107787,
         Title: "Queen Street",
         Description: "Bike Rack",
+        Votes: ['2','3','5','7','11', '13','17','19'],
     },
 
 ];
